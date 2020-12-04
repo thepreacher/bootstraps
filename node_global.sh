e90f2eb5babefddd90e4b4a41231b832b701b743
Napoleon Ahiable <nahiable@MacBook-Pro.local>
2020-12-04 06:09:09 +0000

    cleanup and tweaks

--- node_global.sh
+++ node_global.sh
@@ -24,16 +24,11 @@ pnpm add -g elm-test
 #pnpm add -g create-elm-app
 #pnpm add -g elm-webpack-loader
 
-# purescript
-#pnpm add -g purescript
-#pnpm add -g pulp
-#pnpm add -g psc-package
-
 # typescript
 pnpm add -g typescript
 
 # working with npm
-pnpm add -g yarn
+#pnpm add -g yarn
 #pnpm add -g npx
 #pnpm add -g np
 #pnpm add -g npm-name-cli
@@ -43,9 +38,6 @@ pnpm add -g neovim
 
 # misc
 pnpm add -g spoof # Easily spoof your MAC address in macOS, Windows, & Linux!
-
-# build tools
-pnpm add -g webpack
 
 # Type `git open` to open the GitHub page or website for a repository.
 pnpm add -g git-open
@@ -59,21 +51,18 @@ pnpm add -g diff-so-fancy
 # trash as the safe `rm` alternative
 pnpm add -g trash-cli
 
-# cordova for hybrid mobile apps
+# dev frameworks
+pnpm add -g @quasar/cli
 #pnpm add -g cordova
-
-# framework7 cli
 #pnpm add -g framework7-cli
-pnpm add -g stylelint
-
-# styleling
 
 # formatter and linters
 pnpm add -g prettier
 pnpm add -g eslint
 pnpm add -g js-beautify
+pnpm add -g stylelint
 
-# liver server
+# live server
 pnpm add -g live-server
 
 # language servers
@@ -85,7 +74,12 @@ pnpm add -g vscode-css-languageserver-bin
 pnpm add -g vscode-json-languageserver
 pnpm add -g yaml-language-server
 pnpm add -g indium
+pnpm add -g vls
 
+# build tools
+# Not recommended to install webpack globaly (option -D is same as --save-dev for npm)
+#pnpm add -D webpack
+#pnpm add -D webpack-cli
 
 echo -e "\nPackage installation complete - Don't forget to reshim so that new packages can be recognised"
 echo -e "\n If required run 'pnpm install-completion' to get completions working or pnpm."
